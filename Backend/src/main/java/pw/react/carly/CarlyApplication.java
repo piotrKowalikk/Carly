f8f0e08a6b4cf8196c71108f407359e0701134fd
Tazi34 <pawelkas12345@gmail.com>
2020-01-04 08:30:39 +0100

    init backend

---
+++ Backend/src/main/java/pw/react/carly/CarlyApplication.java
@@ -0,0 +1,13 @@
+package pw.react.carly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlyApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(CarlyApplication.class, args);
+
+    }
+}
